7657c621e5888acdefc2d29aaf91a13a196f68bc
AN095380 <Aayush.Nehra@cerner.com>
2023-04-17 23:56:10 +0530

    Cleaning TextForm Component

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,30 +3,30 @@ import "../App.css"
 
 export default function TextForm(props) {
   //Text Util Functions
-  let convertToUpper = () => {
-    let newText = text;
-    setText(newText.toUpperCase())
+  let convertToUpperCase = () => {
+    let newText = formInputText;
+    setFormText(newText.toUpperCase())
     props.showUtilSuccessMessage("Text converted to upper case");
   }
-  let convertToLower = () => {
-    let newText = text;
-    setText(newText.toLowerCase())
+  let convertToLowerCase = () => {
+    let newText = formInputText;
+    setFormText(newText.toLowerCase())
     props.showUtilSuccessMessage("Text converted to lower case");
   }
   
   let clearText = () => {
     props.showUtilSuccessMessage("Text cleared");
-    setText('')
+    setFormText('')
   }
 
-  let capFirstLetter = ()=> {
-    const finalSentence = text.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
-    setText(finalSentence);
+  let capitailzeFirstLetter = ()=> {
+    const finalSentence = formInputText.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
+    setFormText(finalSentence);
     props.showUtilSuccessMessage("First letter of each word was capitalized");
   }
   
   function WordCounter() {
-    let textAreaText = text.trim()
+    let textAreaText = formInputText.trim()
     let noOfWords = 0;
     if(textAreaText === '')
       noOfWords = 0
@@ -43,9 +43,9 @@ export default function TextForm(props) {
 
   //Allows user to enter value to the text area field
   let setTargetText = (event) => {
-    setText(event.target.value)
+    setFormText(event.target.value)
   }
-  const [text, setText] = useState("Enter some text");
+  const [formInputText, setFormText] = useState("Enter some text");
   return (
     <>
     <div className="container">
@@ -55,21 +55,21 @@ export default function TextForm(props) {
           rows="8"
           id="myBox"
           className={`form-control textarea-${props.mode}`}
-          value={text}
+          value={formInputText}
           onChange={setTargetText}
         ></textarea>
       </div>
-      <button className="btn btn-primary mx-1" disabled={text.length === 0} onClick={convertToUpper}>Upper Case</button>
-      <button className="btn btn-primary mx-1" disabled={text.length === 0} onClick={convertToLower}>Lower Case</button>
-      <button className="btn btn-primary mx-1" disabled={text.length === 0} onClick={capFirstLetter}>Capatalize First Letter</button>
-      <button className="btn btn-primary mx-1" disabled={text.length === 0} onClick={clearText}>Clear Text</button>
+      <button className="btn btn-primary mx-1" disabled={formInputText.length === 0} onClick={convertToUpperCase}>Upper Case</button>
+      <button className="btn btn-primary mx-1" disabled={formInputText.length === 0} onClick={convertToLowerCase}>Lower Case</button>
+      <button className="btn btn-primary mx-1" disabled={formInputText.length === 0} onClick={capitailzeFirstLetter}>Capatalize First Letter</button>
+      <button className="btn btn-primary mx-1" disabled={formInputText.length === 0} onClick={clearText}>Clear Text</button>
     </div>
     <div className={`container my-3`} id={`text-${props.mode}`}>
       <h2>Your Text Summary</h2>
-      <p>Your text has {<WordCounter/>} words and {text.length} characters</p>
-      <p>Time to read is {0.008*text.split(/\s+/).filter(word => word!=='').length} minutes</p>
+      <p>Your text has {<WordCounter/>} words and {formInputText.length} characters</p>
+      <p>Time to read is {0.008*formInputText.split(/\s+/).filter(word => word!=='').length} minutes</p>
       <h2>Text Preview</h2>
-      <p>{text.length === 0?"Enter some text in text box to preview":text}</p>
+      <p>{formInputText.length === 0?"Enter some text in text box to preview":formInputText}</p>
     </div>
     </>
   );
